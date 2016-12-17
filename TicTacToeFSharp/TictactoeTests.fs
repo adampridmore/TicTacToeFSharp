66f42b36735cb78fff9cf09da0b43b3f25846e41
Adam Pridmore <adam_pridmore@hotmail.com>
2016-12-17 22:37:28 +0000

    - Added unit tests  - Upated FSharp.Core to 4.4.0.0  - Added runtime binding redirect.

---
+++ TicTacToeFSharp/TictactoeTests.fs
@@ -0,0 +1,39 @@
+﻿module TictactoeTests
+
+open Tictactoe
+open FsUnit
+open NUnit.Framework
+
+
+[<Test>]
+let a() = 
+    let rowWin = 
+        [| 
+            [| X; X; X |]
+            [| Cell.Empty; Cell.Empty; Cell.Empty |]
+            [| Cell.Empty; Cell.Empty; Cell.Empty |] 
+        |]
+
+    rowWin |> won |> should equal true
+
+
+//
+//let columnWin = 
+//    [| 
+//        [| Empty; Empty; O|]
+//        [| Empty; Empty; O|]
+//        [| Empty; Empty; O|] 
+//    |]
+//    
+//let diagWin = 
+//    [| 
+//        [| Empty; Empty; O|]
+//        [| Empty; O; Empty|]
+//        [| O; Empty; Empty|] 
+//    |]
+//
+//rowWin |> won
+//columnWin |> won
+//diagWin |> won
+//newGame.Cells |> won
+
