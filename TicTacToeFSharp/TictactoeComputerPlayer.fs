a2e4507e6dfe0cab82e16c8177918a680aa51560
Adam Pridmore <adam_pridmore@hotmail.com>
2016-12-23 23:32:53 +0000

    - Ai now plays winning move if there is one.

--- TicTacToeFSharp/TictactoeComputerPlayer.fs
+++ TicTacToeFSharp/TictactoeComputerPlayer.fs
@@ -2,7 +2,24 @@
 open Tictactoe
 
 let getMove (game:Game) = 
-    game 
-    |> getLegalMoves 
-    |> Seq.head
+    let myToken = game.NextMove
+
+    let legalMoves = 
+        game 
+        |> getLegalMoves 
     
+    let isWin (state:GameState) = 
+       match state with 
+       | GameState.XWon when myToken = X -> true
+       | GameState.OWon when myToken = O -> true
+       | _ -> false
+       
+    let winningMoves = 
+        legalMoves 
+        |> Seq.map(fun move -> move, move |> playMove game)
+        |> Seq.filter(fun (_ , nextGameStae) -> nextGameStae.State |> isWin)
+        |> Seq.map fst
+
+    if (winningMoves |> Seq.isEmpty |> not) 
+    then winningMoves |> Seq.head
+    else legalMoves |> Seq.head
