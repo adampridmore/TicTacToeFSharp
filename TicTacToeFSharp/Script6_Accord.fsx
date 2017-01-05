679678d16cba9758bb97f892a248858b2164ac40
Adam Pridmore <adam_pridmore@hotmail.com>
2017-01-05 21:23:54 +0000

    - Made a more complex sample.

--- TicTacToeFSharp/Script6_Accord.fsx
+++ TicTacToeFSharp/Script6_Accord.fsx
@@ -7,40 +7,46 @@ open Accord.Neuro
 open Accord.Neuro.Learning
 
 //Accord.Neuro.Learning.BackPropagationLearning
-
 // http://accord-framework.net/docs/html/T_Accord_Neuro_Learning_BackPropagationLearning.htm
-
 
 // initialize input and output values
 let input = [| 
-    [|0.0;0.0|]
-    [|0.0;1.0|]
-    [|1.0;0.0|]
-    [|1.0;1.0|]
+    [|0.0;0.0;0.0;|]
+    [|0.0;0.0;1.0|]
+    [|0.0;1.0;0.0|]
+    [|0.0;1.0;1.0|]
+    [|1.0;0.0;0.0|]
+    [|1.0;0.0;1.0|]
+    [|1.0;1.0;0.0|]
+    [|1.0;1.0;1.0|]
 |]
 let output = [|
-    [|0.0|]
-    [|1.0|]
-    [|1.0|]
-    [|0.0|]
+    [|1.0;0.0;0.0;|]
+    [|1.0;0.0;0.0;|]
+    [|1.0;0.0;0.0;|]
+    [|1.0;0.0;0.0;|]
+    [|0.0;1.0;0.0;|]
+    [|0.0;1.0;0.0;|]
+    [|0.0;0.0;1.0;|]
+    [|0.0;0.0;0.0;|]
 |]
 
 let network = 
     new ActivationNetwork(
         SigmoidFunction(2.0),
-        2, // two inputs in the network
-        2, // two neurons in the first layer
-        1 
-    ); // one neuron in the second layer
+        3, // number of inputs in the network
+        3, // number of neurons in the first layer
+        3  // number of neuron in the second layer
+    ); 
 
 // create teacher
-let teacher = new BackPropagationLearning( network );
+let teacher = new BackPropagationLearning(network);
 
 // loop
 Seq.initInfinite id
 |> Seq.map (fun _ -> teacher.RunEpoch(input, output))
 |> Seq.takeWhile (fun error -> error > 0.1)
-|> Seq.truncate 100
+|> Seq.truncate 10000
 |> Seq.last
 
-network.Compute([|1.0;1.0|])
+network.Compute([|1.0;1.0;0.0|])
